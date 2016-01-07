acc8d872f2aa0525d68a62004aa7af533259f8c7
Sergey Gladkov <sgladkov@trackingsoft.com>
2016-01-07 23:33:38 +0200

    Redis::CappedCollection 1.03
    Change-Id: I65b89de3701c49b62a0526060540a86db81b1db2

--- lib/Redis/CappedCollection.pm
+++ lib/Redis/CappedCollection.pm
@@ -7,7 +7,7 @@ Redis server setting) collections with FIFO data removal.
 
 =head1 VERSION
 
-This documentation refers to C<Redis::CappedCollection> version 1.02
+This documentation refers to C<Redis::CappedCollection> version 1.03
 
 =cut
 
@@ -20,7 +20,7 @@ use bytes;
 
 # ENVIRONMENT ------------------------------------------------------------------
 
-our $VERSION = '1.02';
+our $VERSION = '1.03';
 
 use Exporter qw(
     import
