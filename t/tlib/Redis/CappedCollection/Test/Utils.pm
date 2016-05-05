82c473cadf8fedeec47414fa3d4bf25fb436dfaf
Sergey Gladkov <sgladkov@trackingsoft.com>
2016-05-05 17:58:41 +0300

    Test corrected
    Change-Id: I906bbdd3249b9a7b2202c7a27c3bcf003d2f969f

--- t/tlib/Redis/CappedCollection/Test/Utils.pm
+++ t/tlib/Redis/CappedCollection/Test/Utils.pm
@@ -65,7 +65,7 @@ sub verify_redis
         eval { $real_redis = Redis->new( server => $DEFAULT_SERVER.":".$port ) };
         $skip_msg = "Redis server is unavailable" unless ( !$@ && $real_redis && $real_redis->ping );
         $skip_msg = "Need a Redis server version 2.8 or higher" if ( !$skip_msg && !eval { return $real_redis->eval( 'return 1', 0 ) } );
-        if ( $real_redis ) {
+        unless ( $skip_msg ) {
             $real_redis->script_flush;
             $real_redis->quit;
         }
